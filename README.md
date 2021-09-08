1f9c70e265b1ea1173b2dfe2e0fcae4cade7a92c
n0 <74681675+LouisTheXIV@users.noreply.github.com>
2021-09-08 12:27:52 +0300

    updated readme

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # PyShortener
+WIP! Do not use yet
 PyShortener is a python 3 package for shortening a url directly from python
 
 # Installation
-open up command prompt and type in  `pip install pyshortener` or `python -m pip install pyshortener`
+`pip install pyshortener` or `python -m pip install pyshortener`
+
+# Usage & Documentation
+Documentation is at: https://pyshortener.readthedocs.io/en/latest/
+
+##### Usage:
+    ```
+    from pyshortener import *
+
+    url_obj = shorten(url="https://youtube.com")
+
+    print(url_obj.link())
+    print(url_obj.code())
+    print(url_obj.html())
+    print(url_obj.status_code)
+    ```
--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 VERSION = '0.0.1'
 DESCRIPTION = 'URL Shortener from python'
-LONG_DESCRIPTION = 'URL Shortener which shortens url directly from your python code. An api for lnk.gdn which is also made by me! All made via requests.'
+LONG_DESCRIPTION = 'URL Shortener which shortens a url directly from your python code. An api for lnk.gdn which is also made by me! All made via requests.'
 
 setup(
     name="pyshortener",
@@ -10,13 +17,13 @@ setup(
     author="JustAnotherCoder",
     author_email="<alexthegreatmatei@gmail.com>",
     description=DESCRIPTION,
-    long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=['requests'],
     keywords=['python', 'requests', 'url shortener', 'url', 'website', 'shortener', 'short', 'request', 'api', 'flask'],
     classifiers=[
-        "Development Status :: 1 - Releasing",
+        "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
